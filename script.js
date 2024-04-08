1da0a0a1acd4764baba4248f56845e8daf31c6cc
CleristonJr <79161552+CleristonJr@users.noreply.github.com>
2024-04-08 18:06:15 -0300

    Inicio do js e alteracoes no html

---
+++ script.js
@@ -0,0 +1,73 @@
+const textOS = document.getElementById("textOS");
+const codigoCriarOS = document.getElementById("codigoCriarOS");
+const radioDesigner = document.getElementById("radioDesigner");
+const radioPlotter = document.getElementById("radioPlotter");
+const radioLaser = document.getElementById("radioLaser");
+const botaoCriar = document.getElementById("botaoCriar");
+const fs = require('fs');
+
+let campos = false;
+let abriu;
+let vaiParaDesigner = false;
+let vaiParaLaser = false;
+let vaiParaPlotter = false;
+
+botaoCriar.addEventListener('click', function() {
+    if (isNaN(textOS.value)) {
+        alert('Por favor, insira apenas valores numéricos no numero da OS.');
+    } else if (isNaN(codigoCriarOS.value)) {
+        alert('Por favor, insira apenas valores numéricos no codigo.');
+    } else if (!textOS.value || !codigoCriarOS.value || !(radioDesigner.checked || radioPlotter.checked || radioLaser.checked)) {
+        alert('Por favor, preencha todos os campos.');
+    } else {
+        campos = true;
+        let dataEspecifica = new Date("2024-04-08T15:03:46 ");
+        console.log(dataEspecifica);
+        console.log(campos);
+        console.log(radioDesigner.checked);
+
+        if(radioDesigner.checked){
+            vaiParaDesigner = true;
+        }else if(radioLaser.checked){
+            vaiParaLaser = true;
+        }else if(radioPlotter.checked){
+            vaiParaPlotter = true;
+        }
+
+        switch (codigoCriarOS.value){
+            case 1001:
+                console.log("O vendedor gerou uma nova OS");
+                abriu = "vendedor";
+            case 1002:
+                console.log("O Gerente gerou uma nova OS");
+                abriu = "gerente";
+            case 1003:
+                console.log("O Desinger gerou uma nova OS");
+                abriu = "designer";
+            case 1004:
+                console.log("O Plotter gerou uma nova OS");
+                abriu = "plotter";
+            case 1005:
+                console.log("O Laser gerou uma nova OS");
+                abriu = "laser";
+        }
+
+        function GravaArquivo(arq, texto) {
+            // pasta a ser salvo o arquivo
+            var pasta = "./site/";
+            // se o parametro arq que é o nome do arquivo vier vazio ele salvará o arquivo com o nome “Sem Titulo”
+            if (arq == textOS.value) { arq = "Sem Titulo"; }
+            // carrega o txt
+            var esc = fs.createWriteStream(pasta + arq + ".txt");
+            // escreve o que foi passado no parametro texto que é o texto contido no TextArea
+            esc.write("Aberta em " + dataEspecifica + "\n");
+            esc.write("Por " + abriu + "\n");
+            esc.write("Vai para Plotter: " + vaiParaPlotter + "\n");
+            esc.write("Vai para Designer: " + vaiParaDesigner + "\n");
+            esc.write("Vai para Laser: " + vaiParaLaser + "\n");
+            // fecha o txt
+            esc.end();
+        }
+
+    }
+});
